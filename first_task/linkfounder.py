f0e1c4fb57bafb99dde3f34ff27677901724218a
Рамиль Нармиев <kalyabushka12@gmail.com>
2024-02-12 15:07:35 +0300

    First task completed

--- first_task/linkfounder.py
+++ first_task/linkfounder.py
@@ -9,7 +9,7 @@ class LinkFounder:
     def __init__(self):
         os.mkdir(os.path.dirname(__file__) + '/pages')
 
-    def find_pages(self):
+    def load_pages(self):
         page = urllib.request.urlopen('https://www.sports.ru/soccer/news/')
         soup = BeautifulSoup(page, 'html.parser')
         links = []
@@ -19,7 +19,7 @@ class LinkFounder:
                 links.append(link)
         return links
 
-    def get_text_from_page(self, url):
+    def filter_text(self, url):
         request = requests.get(url)
         request.encoding = request.apparent_encoding
         if request.status_code == 200:
@@ -30,13 +30,13 @@ class LinkFounder:
             return str(soup)
         return None
 
-    def download_pages(self, count: int):
-        links = list(set(self.find_pages()))
+    def get_links(self, count: int):
+        links = list(set(self.load_pages()))
         index_file = open(os.path.dirname(__file__) + '/index.txt', 'w', encoding='utf-8')
         i = 1
         for link in links:
             if i <= count:
-                text = self.get_text_from_page(link)
+                text = self.filter_text(link)
                 if text is None:
                     continue
                 else:
@@ -53,4 +53,4 @@ class LinkFounder:
 
 if __name__ == '__main__':
     founder = LinkFounder()
-    founder.download_pages(100)
+    founder.get_links(100)
